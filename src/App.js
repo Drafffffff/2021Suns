621d57cec5f550867541a04dfe297a31865c0aff
drafffffff <i0.0ia@qq.com>
2021-05-30 17:19:42 +0800

    hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, useParams } from "react-router-dom";
+import { HashRouter  as Router, Route, useParams } from "react-router-dom";
 
 function App() {
   return (
