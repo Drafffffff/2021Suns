a612137d4b3103a1f8f6120e6b0c0a8497d599ff
drafffffff <i0.0ia@qq.com>
2021-05-30 17:05:05 +0800

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Router>
         <div>
-          <Route path="/:userId">
+          <Route path="2021SunsGraduationEh/:userId">
             <Home />
           </Route>
         </div>
