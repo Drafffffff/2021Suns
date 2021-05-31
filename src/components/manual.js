47102375735a223eab0858b5c4064443a6edaea2
drafffffff <i0.0ia@qq.com>
2021-06-01 05:00:56 +0800

    change

--- src/components/manual.js
+++ src/components/manual.js
@@ -4,16 +4,21 @@
 // https://opensource.org/licenses/MIT
 
 // import React from "react";
-import manImg1 from "../assets/img/1.png";
-import manImg2 from "../assets/img/2.png";
-import manImg3 from "../assets/img/3.png";
+import manImg1 from "../assets/1.png";
+import manImg2 from "../assets/2.png";
+import manImg3 from "../assets/3.png";
+import manImg4 from "../assets/4.png";
+import manImg5 from "../assets/tail.png";
 import styles from "../styles/manual.module.scss";
 function Manual() {
   return (
     <div className={styles.manual}>
-      <img src={manImg1} alt="1" />
+      <img src={manImg1} alt="1" className={styles.m1}/>
+
       <img src={manImg2} alt="2" />
       <img src={manImg3} alt="3" />
+      <img src={manImg4} alt="4" />
+      <img src={manImg5} alt="5" />
     </div>
   );
 }
--- src/mainPage.js
+++ src/mainPage.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import P5canvas from "./components/p5canvas";
-import Manual from "./components/manual"
+import Manual from "./components/manual";
+import styles from "./styles/mainPage.module.scss";
 const AV = require("leancloud-storage");
 function Home() {
   // const [deltTime, setDeltTime] = useState(0);
   const [paramList, setParamList] = useState([]);
-
+  const [callText, setCallText] = useState("");
+  const [callTexten, setCallTexten] = useState("");
+  const [dataText, setDataText] = useState("");
+  const [totalTime, setTotalTime] = useState("");
   let { userId } = useParams();
   const query = new AV.Query("guests");
   // console.log(1);
   let userData;
-  if ((paramList.length === 0)) {
+  if (paramList.length === 0) {
     AV.init({
       appId: "bdGlN6WxWw3YTiT9MIYRxnp6-gzGzoHsz",
       appKey: "I4Rs1qPTJJIo6dj1WMFgBkR9",
@@ -21,7 +25,7 @@ function Home() {
     query.find().then(i => {
       if (i[0]) {
         userData = i[0].attributes;
-
+        console.log(userData);
         let list = [
           userData.a,
           userData.b,
@@ -30,6 +34,31 @@ function Home() {
           userData.e,
           userData.f,
         ];
+        if (userData.textFlag === "0") {
+          setCallText("步履匆匆的行路人");
+          setCallTexten("Walker in rush");
+        } else if (userData.textFlag === "1") {
+          setCallText("闲庭信步的漫游者");
+          setCallTexten("Chill Flâneur");
+        } else if (userData.textFlag === "2") {
+          setCallText("夏日芳香的梦旅人");
+          setCallTexten("Summer Traveller");
+        }
+        if (userData.brithTime) {
+          let date = new Date(Math.floor(userData.brithTime * 1000));
+          let Y = date.getFullYear() + "-";
+          let M =
+            (date.getMonth() + 1 < 10
+              ? "0" + (date.getMonth() + 1)
+              : date.getMonth() + 1) + "-";
+          let D = date.getDate() + " ";
+          let h = date.getHours() + ":";
+          let m = date.getMinutes() + ":";
+          let s = date.getSeconds();
+          setDataText(Y + M + D + h + m + s);
+        }
+        setTotalTime(Math.floor(userData.finishTime - userData.brithTime));
+
         setParamList(list);
       }
     });
@@ -37,11 +66,30 @@ function Home() {
 
   return (
     <div className="Home">
-      <h2>{userId}</h2>
+      {paramList.length > 0 && <Greet userId={userId} />}
+      <h1 className={styles.call}>{callText}</h1>
+      <h1 className={styles.call}>{callTexten}</h1>
+      {paramList.length > 0 && (
+        <p className={styles.pp}>
+          你于{dataText}进入象山艺术公社N59展厅，观展总时长为{totalTime}s;
+        </p>
+      )}
+
       {paramList.length > 0 && <P5canvas paramList={paramList} />}
-      <Manual/>
+      <Manual />
     </div>
   );
 }
+
+let Greet = () => {
+  let { userId } = useParams();
+  return (
+    <div className={styles.greet}>
+      <h2>Helloooo 装配者</h2>
+      <h3>ID: {userId}</h3>
+      <h2>你的观展身份为：</h2>
+    </div>
+  );
+};
 
 export default Home;
